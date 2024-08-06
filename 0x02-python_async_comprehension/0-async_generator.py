49e90b7c954b52a64529c060d9c2663c018be495
HermanMusyoki <salmantso14@gmail.com>
2024-08-06 16:02:36 +0000

    2 Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
-
-""" Module that contains an asynchronous coroutine that takes an integer
-argument (max_delay, with a default value of 10) named wait_random
-that waits for a random delay between 0 and max_delay (included float value)
-seconds and eventually returns it.
-"""
-
+'''Task 0's module.
+'''
 import asyncio
 import random
+from typing import Generator
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    """ Asynchronous coroutine that takes an integer argument
-    and returns it after a delay.
-    """
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-
-    return delay
+async def async_generator() -> Generator[float, None, None]:
+    '''Generates a sequence of 10 numbers.
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
